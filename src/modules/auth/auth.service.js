221e45ad618f5a1ed996d49520eb299fe450b3d7
strawHat <tanuj.sec@outlook.com>
2026-03-24 16:44:03 +0530

    update JWT

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -4,13 +4,11 @@ import { generateResetToken } from "../../common/utils/jwt.token.js";
 
 const register = async ({ name, email, password, role }) => {
   const existing = await User.findone({ email });
-
   if (existing) {
     throw ApiError.conflict("Email already exist");
   }
 
   const { rawToken, hashedToken } = generateResetToken();
-
   const user = await User.create({
     name,
     email,
@@ -18,7 +16,6 @@ const register = async ({ name, email, password, role }) => {
     role,
     verificationToken: hashedToken,
   });
-
   const userObj = user.toObject();
   delete userObj.password;
   delete userObj.verificationToken;
